11df9a44b4953989c6fb90c8051ce867b39cf6a2
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-08-25 17:52:45 +0530

    initialised

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button variant="slack">Click me</Button>
+    </div>
+  );
+}
+ 
