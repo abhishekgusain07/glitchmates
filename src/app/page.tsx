92522c55ff926930f551b2a5f980d9e42727f309
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-08-25 20:20:12 +0530

    feat: logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 'use client'
-
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
 import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
   const {signOut} = useAuthActions()
+
   return (
     <div>
-      Logged In!
-      <Button onClick={() => signOut()}>Sign Out</Button>
+      <UserButton />
     </div>
   );
 }
---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { 
+    Avatar,
+    AvatarFallback,
+    AvatarImage
+} from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuGroup,
+    DropdownMenuItem
+} from "@/components/ui/dropdown-menu";
+import { Loader, LogOut, UserIcon } from "lucide-react";
+import { useCurrentUser } from "../hooks/user-current-user";
+import { useAuthActions } from "@convex-dev/auth/react";
+
+export const UserButton = () => {
+    const {data, isLoading} = useCurrentUser()
+    const {signOut} = useAuthActions();
+    console.log(data)
+    if(isLoading) {
+        return <Loader className="size-4 animate-spin text-muted-foreground" />
+    }
+    if(!data) {
+        return null;
+    }
+    const {image, name, email} = data;
+    const avatarFallback = name!.charAt(0).toUpperCase()
+    console.log(data)
+    return (
+        <DropdownMenu modal={false}>
+            <DropdownMenuTrigger className="outline-none relative">
+                <Avatar className="size-10 hover:opacity-75 transition">
+                    <AvatarImage src={image} alt={name!} />
+                    <AvatarFallback className="bg-blue-400 text-white">
+                        {avatarFallback}
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="center" side="right" className="w-60">
+                <DropdownMenuItem className="h-10" onClick={() => signOut()} >
+                    <LogOut className="size-4 mr-2" />
+                    Logout
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
