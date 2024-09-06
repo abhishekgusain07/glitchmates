dafffc959d84dad93f12e0b14f23dba649758667
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2024-09-06 10:15:22 +0530

    fix: styling

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -43,9 +43,9 @@ const Toolbar = () => {
         router.push(`/workspace/${workspaceId}/member/${memberId}`);
     }
     return (
-        <div className="bg-bgCustom2 flex items-center justify-between h-10 p-1.5">
+        <div className="bg-bgCustom2/70 flex items-center justify-between h-10 p-1.5">
             <div className="flex-1" />
-            <div className="min-w-[280px] max-[642px] grow-[2] shrink">
+            <div className="min-w-[280px] max-[642px] grow-[2] shrink  ml-0 md:ml-[3rem]">
                 <Button 
                 size="sm" 
                 className="bg-accent/25 hover:bg-accent-25 w-full justify-start h-7 px-2"
@@ -86,7 +86,6 @@ const Toolbar = () => {
                 <Button size="iconSm" variant="transparent">
                     <Info className="size-5 text-white" />
                 </Button> 
-
             </div>
         </div>
     )
